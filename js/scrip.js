0324493a1dd0b7fa771a7cbce1f21464d7d8ba0c
Scardonasuarez <santicardonal777@icloud.com>
2023-12-19 15:20:03 -0500

    creacios de preguntas en JS

--- js/scrip.js
+++ js/scrip.js
@@ -1,5 +1,17 @@
 const busqueda = [
 
-    
+    {
+        nombre: '',
+        genero: '',
+        edad: 0,
+        gustoMusical: '',
+        hobbies: '',
+        favoriteFood: '',
+        color: '',
+        barrio: '',
+        mascotas: '',
+        favoriteMovie: '',
+        gustosPersonales: ''
+    }
 
 ]
